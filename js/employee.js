75c86511e2927617e8f69b5ebcf3bc433d1cae3b
gugnanihondanayagarh-max <gugnanihonda.nayagarh@gmail.com>
2026-07-16 12:01:30 +0530

    Update employee.js

--- js/employee.js
+++ js/employee.js
@@ -730,10 +730,14 @@ const EmployeeApp = {
                     statusClass = "half";
                     statusLetter = `<span class="badge bg-warning text-dark rounded-circle shadow-sm d-flex align-items-center justify-content-center mx-auto" style="width: 22px; height: 22px; font-size:0.6rem; padding: 0; margin-top:2px; font-weight: bold;">IN</span>`;
                     statusTooltip = `Late Arrival`;
-                } else if (status.includes("Half")) {
+                } else if (status.includes("Short")) {
                     statusClass = "half";
                     statusLetter = `<span class="badge bg-warning text-dark rounded-circle shadow-sm d-flex align-items-center justify-content-center mx-auto" style="width: 22px; height: 22px; font-size:0.65rem; padding: 0; margin-top:2px; font-weight: bold;">P</span>`;
-                    statusTooltip = `Half Day / Early Go (Status: ${status})`;
+                    statusTooltip = `Short Day (Status: ${status})`;
+                } else if (status.includes("Half")) {
+                    statusClass = "half";
+                    statusLetter = `<span class="badge bg-warning text-dark rounded-circle shadow-sm d-flex align-items-center justify-content-center mx-auto" style="width: 22px; height: 22px; font-size:0.65rem; padding: 0; margin-top:2px; font-weight: bold;">H</span>`;
+                    statusTooltip = `Half Day (Status: ${status})`;
                 } else if (status.includes("Present") || status.includes("Completed") || status.includes("Manual")) {
                     statusClass = "present";
                     statusLetter = "P";
